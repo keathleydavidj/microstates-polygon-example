d83b7915a1c54849f65ede0b4a30e2ddacd3d3a9
David Keathley <david@frontside.io>
2018-05-04 17:20:34 -0500

    Add Polygon Microstate with sides property

---
+++ src/polygon.js
@@ -0,0 +1,17 @@
+
+export default class Polygon {
+  sides = Number;
+  sides2 = [Number];
+  sideLength = Number;
+
+  incrementSides(value = 1) {
+    if (this.state.sides + value < 3) {
+      return this;
+    } else {
+      return this.sides.increment(value);
+    }
+  }
+  get canDecrement() {
+    return this.sides > 3;
+  }
+}
